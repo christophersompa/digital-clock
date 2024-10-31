a0a36d30c70ca8260c1794739a028a189293e046
Christopher <cgtsompa@gmail.com>
2024-10-31 21:04:31 +0000

    use strict!

---
+++ scripts.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/* "use strict" is a literal expression that indictes 
+ * that the code shoudl be executed in "strict mode".
+ * 
+ * Strict mode makess it easier to write "secure" javacript,
+ * strict mode changes previously accepted "bad syntax" into 
+ * real errors.
+ * 
+ * For example, mistyping a variable name creates a new
+ * glabal variable. In strict mode, this will throw an error,
+ * making it impossible to accidently create a global varibale.
+ * 
+ * this should be used at the top of the script file.
+ */
