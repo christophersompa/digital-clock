b97c843b8edde1441c73d010f9fc60e22a39c458
Christopher <cgtsompa@gmail.com>
2024-11-02 20:33:30 +0000

    variables

--- scripts.js
+++ scripts.js
@@ -13,3 +13,54 @@
  * 
  * this should be used at the top of the script file.
  */
+
+/* A variable is a "named storage" for data. We can use variables
+to store goodies, visitors, and other data. To create a variable in
+JavaScript, use the "let" keyword. The statement below creates 
+(in other words: declares) a variable with the name "message":*/
+
+let message; 
+
+/* We can put some data into it by using the assignmrnt operator "=" */
+
+message = "Hello";
+
+/* The string is now saved into the memory area associated with
+the variable. We can access it using the variable name: */
+
+alert(message);
+
+/* We can also declare multiple variables in one line 
+(this is not recommended, for the sake of better readability, please
+use a single line per variable): */
+
+/*
+let user = "Chris", age = 26, job = "frontend developer"; 
+*/
+
+let user = "Chris";
+let age = 26;
+let job = "frontend developer";
+
+/* We can easily grasp the concept of a "variable" if we imagine it as
+a "box" for data, with a uniquely-named sticker on it. For instance,
+the variable "message" can be imagined as a box labelled "message"
+with the value "Hello" in it. */
+
+/* We can also change it as many times as we want */
+
+job = "backend developer";
+
+/* We can also declare two variables and copy data from one into
+the other: */
+
+let job_sector; 
+
+job_sector = job;
+
+/* There are two limitations on variable names in JavaScript: The 
+name must contain only letters, digits, or the symbol "$" and "_". And
+the first character must not be a digit. Example:*/
+
+let userName;
+let test123;
