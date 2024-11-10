006dc8e90a3f2b7643924e5a7fd8bdb592e38950
Christopher <cgtsompa@gmail.com>
2024-11-10 22:01:42 +0000

    Maths and numbers part 1

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
 // JavaScript Tutorial Full Course - Beginner to Pro (2024) Documentation
 
+//                       Maths and Numbers Syntax
 
+// We write it down like normal math e.g. 9 + 1, 4 * 2, 8 / 2 and 5 - 3 
+
+// The order of operation * & / are done first and + & - are done after. This is called operator precedence.
+
+// If you have 2 * 3 / 2 - it would calculate left to right.
+
+// We can use brackets to control which gets done first (1 + 1) * 3 = 6
+
+// Brackets have the highest priority in operator precedence
